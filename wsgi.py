fefb9df3c635d533ac30d45b4666a23a3a0149b6
mmginstructor <kiosk@foundation0.ilt.example.com>
2020-07-13 18:08:28 +0200

    2 modificacion

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "os.environ["USER"] - Hello World!"
+    return "os.environ[\"USER\"] - Hello World!"
 
 if __name__ == "__main__":
     application.run()
